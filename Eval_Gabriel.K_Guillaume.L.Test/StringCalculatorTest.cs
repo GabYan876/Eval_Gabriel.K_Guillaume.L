6e49a01c51fd5f1f8bdf16e976ba99c61ef4a072
GabYan <gabyan877@gmail.com>
2024-03-07 10:20:34 +0100

    [REFACTO] Test1 -> StringEstEgalAUn

--- Eval_Gabriel.K_Guillaume.L.Test/StringCalculatorTest.cs
+++ Eval_Gabriel.K_Guillaume.L.Test/StringCalculatorTest.cs
@@ -3,12 +3,16 @@ namespace Eval_Gabriel.K_Guillaume.L.Test
     public class StringCalculatorTest
     {
         [Fact]
-        public void Test1()
+        public void StringEstEgalAUn()
         {
+
+            //ETANT DONNE un string "1"
             const string input = "1";
 
+            //QUAND on utilise la methode Parse
             var result = StringCalculator.Parse(input);
 
+            //ALORS le string "1" devient l'int 1
             Assert.Equal(1, result);
         }
     }
