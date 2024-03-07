ff544a227a9d9fa4f53b0a061ec6baf307a855a9
GabYan <gabyan877@gmail.com>
2024-03-07 10:25:27 +0100

    [RED] StringDeDeuxChiffres

--- Eval_Gabriel.K_Guillaume.L.Test/StringCalculatorTest.cs
+++ Eval_Gabriel.K_Guillaume.L.Test/StringCalculatorTest.cs
@@ -29,5 +29,19 @@ namespace Eval_Gabriel.K_Guillaume.L.Test
             //ALORS le string "1" devient l'int 1
             Assert.Equal(2, result);
         }
+        
+        [Fact]
+        public void StringDeDeuxChiffres()
+        {
+
+            //ETANT DONNE un string "1,2"
+            const string input = "1,2";
+
+            //QUAND on utilise la methode Parse
+            var result = StringCalculator.Parse(input);
+
+            //ALORS le string "1,2" devient l'int 3
+            Assert.Equal(3, result);
+        }
     }
 }
